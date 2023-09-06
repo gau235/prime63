38e6a1cd0148d569edd91e00954ee7219e9d6286
gau235 <gau235@gmail.com>
2023-09-06 13:07:36 +0800

    t4

--- notJavaFile/python/distinct.py
+++ notJavaFile/python/distinct.py
@@ -1,24 +1,20 @@
 # trim the sorted ary
-list=[2,2,3,7,7,7,9,9,10,10]
-print("old list=", list);
+oldList=[2,2,3,7,7,7,9,9,10,10]
+print("old oldList=", oldList);
 
 count=1
-max=list[0]
-for val in list: # list 必須是遞增數列
-	if (val > max):
-		list[count]=val
-		print("val="+str(val)+" count="+str(count) +" max="+str(max), end='\n')
+max=oldList[0]
+
+for v in oldList: # oldList 必須是遞增數列
+	if v > max:
+		oldList[count]=v
+		print("v="+str(v)+" count="+str(count) +" max="+str(max), end='\n')
 		count+=1
-		max=val
+		max=v
 
-
-print("old list=", list)
-
-newLen=len(list)
-if (newLen > count): # 不重複的值只占用 list 前幾個位置
-	for val in range(count, newLen): # trim tail
-		print("pop ", list.pop())
-		
-
-print("count=", count)
-print("new list=", list)
+print("oldList="+str(oldList))
+newLen=len(oldList)
+if newLen > count: # 不重複的值只占用 oldList 前幾個位置
+	for v in range(count, newLen): print("pop="+str(oldList.pop())) # trim tail
+#####################################
+print("newList="+str(oldList)+" len="+str(count))
